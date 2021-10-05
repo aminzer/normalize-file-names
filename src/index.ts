c446aeee5dee254f27df10559e1a60a4a8786e3c
aminzer <minzer.alexey@gmail.com>
2021-10-05 21:55:53 +0300

    update progress logging

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as path from 'path';
 import { iterateDirectoryChildren } from '@aminzer/dir-diff';
-import { log } from './logger';
+import { log, logSingleLine } from './logger';
 import {
   getCreationTimeFromFileName,
   getOutputFileName,
@@ -31,31 +31,51 @@ async function processFile(sourceFilePath: string, targetFilePath: string): Prom
     await ensureDirExists(unrecognizedFilesOutputDirPath);
 
     const totalFileCount = await getFileCount(inputDirPath);
-    log(`Total file count: ${totalFileCount}`);
+    let recognizedFileCount = 0;
+    let unrecognizedFileCount = 0;
+
+    const logProgress = () => {
+      const processedFileCount = recognizedFileCount + unrecognizedFileCount;
+      const progressPercentage = Math.round(
+        100 * (totalFileCount > 0 ? processedFileCount / totalFileCount : 1),
+      );
+
+      logSingleLine(`Processed files: ${processedFileCount}/${totalFileCount} (${progressPercentage}%)`);
+    };
+
+    const loggingIntervalId = setInterval(logProgress, 200);
 
     await iterateDirectoryChildren(inputDirPath, async (fsEntry) => {
       if (fsEntry.isDirectory) {
         return;
       }
 
+      let creationTime;
+      let targetDirPath;
+
       const creationTimeFromFileName = getCreationTimeFromFileName(fsEntry.name);
 
       if (creationTimeFromFileName !== null) {
-        const outputFileName = getOutputFileName(fsEntry.name, creationTimeFromFileName);
-        const outputFilePath = path.join(outputDirPath, outputFileName);
-
-        await processFile(fsEntry.absolutePath, outputFilePath);
-        return;
+        creationTime = creationTimeFromFileName;
+        targetDirPath = outputDirPath;
+        recognizedFileCount += 1;
+      } else {
+        creationTime = await getCreationTimeFromFs(fsEntry.absolutePath);
+        targetDirPath = unrecognizedFilesOutputDirPath;
+        unrecognizedFileCount += 1;
       }
 
-      const creationTimeFromFs = await getCreationTimeFromFs(fsEntry.absolutePath);
-      const outputFileName = getOutputFileName(fsEntry.name, creationTimeFromFs);
-      const outputFilePath = path.join(unrecognizedFilesOutputDirPath, outputFileName);
+      const outputFileName = getOutputFileName(fsEntry.name, creationTime);
+      const outputFilePath = path.join(targetDirPath, outputFileName);
 
       await processFile(fsEntry.absolutePath, outputFilePath);
     });
 
-    log('Done');
+    clearInterval(loggingIntervalId);
+    logProgress();
+    log();
+    log(`Recognized files: ${recognizedFileCount}`);
+    log(`Unrecognized files: ${unrecognizedFileCount}`);
   } catch (err) {
     log(err);
   }
--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -2,4 +2,3 @@ import { stdout as singleLineLog } from 'single-line-log';
 
 export const { log } = console;
 export const logSingleLine = singleLineLog;
-export const clearSingleLine = singleLineLog.clear;
