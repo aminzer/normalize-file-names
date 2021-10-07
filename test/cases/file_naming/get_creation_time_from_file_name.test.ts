6057caf9e5a1fba06a58c00152d27e260b37e9cb
aminzer <minzer.alexey@gmail.com>
2021-10-07 17:03:29 +0300

    refactor test cases

--- test/cases/file_naming/get_creation_time_from_file_name.test.ts
+++ test/cases/file_naming/get_creation_time_from_file_name.test.ts
@@ -1,11 +1,28 @@
 import { getCreationTimeFromFileName } from '../../../dist/file_naming';
 
-describe('getCreationTimeFromFileName', () => {
-  it('parses concatenated date and time', () => {
-    expect(getCreationTimeFromFileName('20200101102030.jpg')).toBe(new Date('2020-01-01 10:20:30').valueOf());
-  });
+type Case = {
+  input: string;
+  output: string | number;
+};
 
-  it('parses timestamp', () => {
-    expect(getCreationTimeFromFileName('1600000000000.jpg')).toBe(1600000000000);
+const cases: Case[] = [{
+  input: '20010203040506',
+  output: '2001-02-03 04:05:06',
+}, {
+  input: '1600000000000',
+  output: new Date(1600000000000).valueOf(),
+}];
+
+describe('getCreationTimeFromFileName', () => {
+  cases.forEach(({ input, output }) => {
+    const fileName = `${input}.jpg`;
+
+    describe(`when input file is "${fileName}"`, () => {
+      it(`return timestamp of ${output}`, () => {
+        const expectedTime = new Date(output).valueOf();
+
+        expect(getCreationTimeFromFileName(fileName)).toBe(expectedTime);
+      });
+    });
   });
 });
