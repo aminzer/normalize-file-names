f082340d5c350ae7a36aaad2639748fd8fa414cf
aminzer <minzer.alexey@gmail.com>
2021-10-08 12:54:49 +0300

    simplify regexes; add test cases

--- src/time_parsers/time_parsers.ts
+++ src/time_parsers/time_parsers.ts
@@ -5,11 +5,11 @@ const sep = '[ ._-]';
 
 const timeParsers: TimeParser[] = [
   (input: string): number => {
-    const match = input.match(/.*(\d{14}).*/);
+    const match = input.match(/(\d{14})/);
     return match ? parseDate(match[1], 'yyyyMMddHHmmss') : null;
   },
   (input: string): number => {
-    const match = input.match(new RegExp(`.*(\\d{8})${sep}(\\d{6}).*`));
+    const match = input.match(new RegExp(`(\\d{8})${sep}(\\d{6})`));
     return match ? parseDate(match[1] + match[2], 'yyyyMMddHHmmss') : null;
   },
   (input: string): number => (/^\d+$/.test(input) ? +input : null),
